60f5661b53a17c5e69ff6f08d80239771bca8dc5
rahul-codeg2 <rahul.hardwin@gmail.com>
2023-12-01 15:10:51 +0530

    minor changes

--- src/main/java/com/ecomm_product_service/EcommProductServiceApplication.java
+++ src/main/java/com/ecomm_product_service/EcommProductServiceApplication.java
@@ -2,10 +2,16 @@ package com.ecomm_product_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommProductServiceApplication {
+public class EcommProductServiceApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EcommProductServiceApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(EcommProductServiceApplication.class, args);
 	}
